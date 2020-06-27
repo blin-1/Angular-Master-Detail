9bc490f84bcabf01a35888e77b0cb959cc0bdd69
blin-1 <y.monsoon@gmail.com>
2020-06-26 21:27:34 -0400

    @output added

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,12 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Detail } from 'src/app/models/detail';
 
 @Component({
   selector: 'app-detail',
-  template: 'id: {{detail.id}} <br> value: {{detail.value}}'
+  template: 'Selected in Parent Component' +
+            '<br>' +
+            'id: {{detail.id}} <br>' +
+            'value: {{detail.value}}' +
+            '<br><br>' +
+            '<button (click)="onSelect(detail)"> Send this to Parent </button>'
 })
 export class DetailComponent {
 
-  @Input() detail: Detail;
+  @Input()  detail: Detail;
+  @Output() detailDispatched: EventEmitter <Detail> = new EventEmitter();
 
+  private onSelect (detail: Detail) {
+    this.detailDispatched.emit(detail);
+  }
 }
--- src/app/master/master.component.ts
+++ src/app/master/master.component.ts
@@ -8,17 +8,31 @@ import { Detail } from 'src/app/models/detail';
 
 export class MasterComponent  {
 
-  details: Array<Detail> = [{id: 1, value: 'row1'},
-                            {id: 2, value: 'row2'},
-                            {id: 3, value: 'row3'}
+  details: Array<Detail> = [{id: 1, value: 'Apples'},
+                            {id: 2, value: 'Oranges'},
+                            {id: 3, value: 'Pears'}
                           ];
   headers: Array<string> = ['id', 'value'];
 
   selectedDetail: Detail = new Detail();
+  childMessage: String  = null;
 
-  onSelect(detail: Detail) {
+  private onDetailSelect(detail: Detail) {
 
     this.selectedDetail = detail;
 
   }
+
+  private onDetailDispatched (detail: Detail) {
+
+    this.childMessage = ' {id: ' + detail.id + ', value: ' + detail.value +  '} was clicked in Child Component';
+
+  }
+
 }
+
+
+
+                // (stockValueChange)='changeStockValue($event)'
+
+
